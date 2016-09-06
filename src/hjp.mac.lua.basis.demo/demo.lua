e87070a2d14999cfa17b2d2da3baf29c91474b78
hjpwhu <hjpwhu@qq.com>
2016-09-06 15:25:57 +0800

    ini

--- src/hjp.mac.lua.basis.demo/demo.lua
+++ src/hjp.mac.lua.basis.demo/demo.lua
@@ -1,1 +1,16 @@
+-- Lua demo.
 print("Hello, Lua!")
+
+-- Variables and Printing.
+a, b = 24, "tacos"
+c = 'please'
+print(a, b, c, "\n")
+d = b .. ', ' .. c
+print(d)
+
+-- Scalar Math
+print(2*1, a^2, a%2, "\n")
+print(a/7, "\n")
+print(math.floor(a/7), math.ceil(a/7), "\n")
+print(math.min(1, 22, 44), math.max(1, 22, 44), "\n")
+print()
